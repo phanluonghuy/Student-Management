ac826839d860000f4c4142310a556bad09196835
Huy Phan Luong <phanhuy04062003@gmail.com>
2023-12-11 00:11:05 +0700

    complete Certificate + Update db

--- src/main/java/DAO/CertificatesDAO.java
+++ src/main/java/DAO/CertificatesDAO.java
@@ -1,6 +1,6 @@
 package DAO;
 
-import Model.Certificates;
+import Model.Certificate;
 
 import javax.swing.*;
 import java.sql.Connection;
@@ -13,11 +13,13 @@ import java.util.List;
 
 public class CertificatesDAO {
     private DatabaseRepository databaseRepository;
-    private static final String GET_CERTIFICATES = "SELECT c.*, clf.classification_name\n" +
-            "FROM certificates c\n" +
-            "JOIN classifications clf ON c.classification_id = clf.classification_id\n" +
-            "WHERE c.student_id = ?;";
+    private static final String GET_CERTIFICATES = "SELECT * FROM certificates WHERE student_id = ?";
     private static final String DELETE_CERTIFICATE = "DELETE FROM certificates WHERE certificate_id = ?";
+    private static final String ADD_CERTIFICATE = "INSERT INTO `certificates` (`student_id`, `date_create`, `certificate_name`, `certificate_level`, `expired_date`) \n" +
+            "VALUES (?, ?, ?, ?, ?);";
+    private static final String UPDATE_CERTIFICATE = "UPDATE `certificates` \n" +
+            "SET `certificate_name` = ?, `certificate_level` = ?, `expired_date` = ? \n" +
+            "WHERE `certificate_id` = ?;";
     public CertificatesDAO() {
         this.databaseRepository = new DatabaseRepository();
     }
@@ -30,28 +32,27 @@ public class CertificatesDAO {
         databaseRepository.closeConnection(conn);
     }
 
-    public List<Certificates> getCertificates(String studentID) throws SQLException {
-        List<Certificates> certificatesList = new ArrayList<>();
+    public List<Certificate> getCertificates(String studentID) throws SQLException {
+        List<Certificate> certificateList = new ArrayList<>();
         try (Connection connection = getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(GET_CERTIFICATES);
             preparedStatement.setString(1, studentID);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 String certificate_id = resultSet.getString("certificate_id");
-                String classification_id = resultSet.getString("classification_id");
                 String student_id = resultSet.getString("student_id");
-                String account_id = resultSet.getString("account_id");
                 Date date_created = resultSet.getDate("date_create");
                 String certificate_name = resultSet.getString("certificate_name");
-                String classification_name = resultSet.getString("classification_name");
-                certificatesList.add(new Certificates(certificate_id, classification_id, student_id, account_id, date_created, certificate_name, classification_name));
+                String certificate_level = resultSet.getString("certificate_level");
+                Date expired_date = resultSet.getDate("expired_date");
+                certificateList.add(new Certificate(certificate_id, student_id, date_created, certificate_name, certificate_level,expired_date));
             }
             CloseConnection(connection);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return certificatesList;
+        return certificateList;
     }
     public boolean deleteCertificate(String certificateID) {
         try (Connection connection = getConnection())
@@ -64,6 +65,54 @@ public class CertificatesDAO {
                 return true;
             } else {
                 JOptionPane.showMessageDialog(null, "Delete Certificate Failed", "Delete Failed", JOptionPane.ERROR_MESSAGE);
+                return false;
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean addCertificate(Certificate certificate) {
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(ADD_CERTIFICATE);
+            java.sql.Date currentDate = new java.sql.Date(System.currentTimeMillis());
+
+            statement.setString(1,certificate.getStudent_id());
+            statement.setDate(2,currentDate);
+            statement.setString(3,certificate.getCertificate_name());
+            statement.setString(4,certificate.getCertificate_level());
+            statement.setDate(5,new java.sql.Date(certificate.getExpired_date().getTime()));
+            int result = statement.executeUpdate();
+            if (result>0) {
+                JOptionPane.showMessageDialog(null, "Add Certificate Successfully", "Add Completed", JOptionPane.INFORMATION_MESSAGE);
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Add Certificate Failed", "Add Failed", JOptionPane.ERROR_MESSAGE);
+                return false;
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean updateCertificate(Certificate certificate) {
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(UPDATE_CERTIFICATE);
+            java.util.Date utilDate = certificate.getExpired_date();
+
+            statement.setString(1,certificate.getCertificate_name());
+            statement.setString(2,certificate.getCertificate_level());
+            statement.setDate(3,new java.sql.Date(certificate.getExpired_date().getTime()));
+            statement.setString(4,certificate.getCertificate_id());
+
+            int result = statement.executeUpdate();
+            if (result>0) {
+                JOptionPane.showMessageDialog(null, "Update Certificate Successfully", "Update Completed", JOptionPane.INFORMATION_MESSAGE);
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "Update Certificate Failed", "Update Failed", JOptionPane.ERROR_MESSAGE);
                 return false;
             }
         } catch (SQLException e){
